0bcd5a04259397d560f2969b74aff775b118559d
Chris-Dong <ydongbd@connect.ust.hk>
2025-08-04 21:48:51 +0800

    add 2.0.0 model

---
+++ 2.0.0/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:93bf04361a0b36d653a9e26ac0d19ce023ec58a100ee8b4a07ccf673deacef36
+size 2053
---
+++ 2.0.0/test_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1fe152d6df4e48b32d1159e95397a6d8d00ab96974104897ce764e42d4b5a29
+size 2877
